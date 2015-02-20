245bf13e5ddfd752ab29927f2298d20a04ad67d0
Nic Nilov <nicnilov@gmail.com>
2015-02-20 23:29:56 +0300

    add flickr account button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,17 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def current_flickr_account
+    if @current_flickr_account.present? && session[:flickr_account_id] == @current_flickr_account.id
+      @current_flickr_account
+    elsif current_user.present?
+      @current_flickr_account = current_user.flickr_accounts.find_by_id(session[:flickr_account_id]) if session[:flickr_account_id].present?
+      unless @current_flickr_account.present?
+        @current_flickr_account = current_user.flickr_accounts.first
+        session[:flickr_account_id] = @current_flickr_account.id if @current_flickr_account.present?
+      end
+    end
+  end
+  helper_method :current_flickr_account
 end
