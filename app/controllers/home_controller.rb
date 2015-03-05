c9eb7a03ff8e76020d6ad3e6bbcd85dadc12296d
Nic Nilov <nicnilov@gmail.com>
2015-03-05 21:43:18 +0300

    album thumbnails styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,13 @@ class HomeController < ApplicationController
     if current_flickr_account.present?
       flickr_account = FlickrAccount.find(current_flickr_account)
       flickr_api = flickrapi(flickr_account.oauth_token, flickr_account.oauth_token_secret)
-      @albums = flickr_api.list_albums
+      @albums = flickr_api.list_albums.collect do |album|
+        if album['videos'].to_i > 0
+          album['background_image'] = "https://farm#{album['farm']}.staticflickr.com/#{album['server']}/#{album['primary']}_#{album['secret']}_q.jpg"
+          album['stats'] = "#{album['videos']} videos"
+          album
+        end
+      end.compact
     end
   end
 end
